453f3db30c952c4477e59b6213e29b7173bafb8f
src_resources <src.res.211@gmail.com>
2025-04-08 17:23:13 +0800

    Fix duplicating items bug
    Fix duplicating items bug when outputting is not available.

--- src/main/kotlin/top/srcres258/bonsaicrops/util/LootTableHelper.kt
+++ src/main/kotlin/top/srcres258/bonsaicrops/util/LootTableHelper.kt
@@ -6,7 +6,6 @@ import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockBehaviour
 import net.minecraft.world.level.block.state.BlockState
-import net.minecraft.world.level.storage.loot.BuiltInLootTables
 import net.minecraft.world.level.storage.loot.LootParams
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams
